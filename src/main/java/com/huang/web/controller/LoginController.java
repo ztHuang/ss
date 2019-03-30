3d9fa456896d3a2c1f872ec2adb4ceb7b0b41610
huangzt <15915686069@163.com>
2019-03-30 16:06:40 +0800

    使用Validator加注解，去掉繁琐的判断

--- src/main/java/com/huang/web/controller/LoginController.java
+++ src/main/java/com/huang/web/controller/LoginController.java
@@ -3,15 +3,15 @@ package com.huang.web.controller;
 import com.huang.web.result.CodeMsg;
 import com.huang.web.result.Result;
 import com.huang.web.service.SSUserService;
-import com.huang.web.util.ValidatorUtil;
 import com.huang.web.vo.LoginVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 /**
  * @Description 登陆
@@ -36,20 +36,20 @@ public class LoginController {
 
     @RequestMapping("/do_login")
     @ResponseBody
-    public Result<Boolean> doLogin(LoginVo loginVo) {
+    public Result<Boolean> doLogin(@Valid LoginVo loginVo) {
         log.info(loginVo.toString());
         //参数校验
-        String inputPassword = loginVo.getPassword();
-        String mobile = loginVo.getMobile();
-        if (StringUtils.isEmpty(inputPassword)) {
-            return Result.error(CodeMsg.PASSWORD_EMPTY);
-        }
-        if (StringUtils.isEmpty(mobile)) {
-            return Result.error(CodeMsg.MOBILE_EMPTY);
-        }
-        if (!ValidatorUtil.isMobile(mobile)) {
-            return Result.error(CodeMsg.MOBILE_ERROR);
-        }
+        //String inputPassword = loginVo.getPassword();
+        //String mobile = loginVo.getMobile();
+        //if (StringUtils.isEmpty(inputPassword)) {
+        //    return Result.error(CodeMsg.PASSWORD_EMPTY);
+        //}
+        //if (StringUtils.isEmpty(mobile)) {
+        //    return Result.error(CodeMsg.MOBILE_EMPTY);
+        //}
+        //if (!ValidatorUtil.isMobile(mobile)) {
+        //    return Result.error(CodeMsg.MOBILE_ERROR);
+        //}
 
         //验证成功，登陆
         CodeMsg codeMsg = ssUserService.login(loginVo);
--- src/main/java/com/huang/web/vo/LoginVo.java
+++ src/main/java/com/huang/web/vo/LoginVo.java
@@ -1,7 +1,11 @@
 package com.huang.web.vo;
 
+import com.huang.web.validator.IsMobile;
 import lombok.Data;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * @Description 页面传回数据
@@ -12,6 +16,11 @@ import lombok.ToString;
 @Data
 @ToString
 public class LoginVo {
+    @NotNull
+    @IsMobile
     private String mobile;
+
+    @NotNull
+    @Length(min = 32)
     private String password;
 }
